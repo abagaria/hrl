8ed5711439341b1d75314ab4f3a8d41169dbeef0
Paul Zhou <paul_zhou@brown.edu>
2021-08-03 22:38:10 -0400

    enable private attribute forwarding in VectorEnvWrapper

--- hrl/wrappers/vector_env_wrapper.py
+++ hrl/wrappers/vector_env_wrapper.py
@@ -7,10 +7,10 @@ class VectorEnvWrapper:
 
 	# implicitly forward all other methods and attributes to self.env
 	def __getattr__(self, name):
-		if name.startswith("_"):
-			raise AttributeError(
-				"attempted to get missing private attribute '{}'".format(name)
-			)
+		# if name.startswith("_"):
+		# 	raise AttributeError(
+		# 		"attempted to get missing private attribute '{}'".format(name)
+		# 	)
 		return getattr(self.env, name)
 
 	def __repr__(self):
