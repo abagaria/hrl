64c0f2e2afeddba6cc5b8a25170450913f83b344
Paul Zhou <paul_zhou@brown.edu>
2021-09-13 17:18:41 -0400

    rename 'goal_state' files to 'middle_ladder'

--- hrl/option/train_option.py
+++ hrl/option/train_option.py
@@ -100,10 +100,10 @@ class TrainOptionTrial:
         # set up env and its goal
         self.env = self.make_env(self.params['environment'], self.params['seed'])
         if self.params['agent_space']:
-            goal_state_path = os.path.join(self.params['goal_state_dir'], 'agent_space_goal_state.npy')
+            goal_state_path = os.path.join(self.params['goal_state_dir'], 'middle_ladder_agent_space.npy')
         else:
-            goal_state_path = os.path.join(self.params['goal_state_dir'], 'goal_state.npy')
-        goal_state_pos_path = os.path.join(self.params['goal_state_dir'], 'goal_state_pos.txt')
+            goal_state_path = os.path.join(self.params['goal_state_dir'], 'middle_ladder.npy')
+        goal_state_pos_path = os.path.join(self.params['goal_state_dir'], 'middle_ladder_pos.txt')
         self.params['goal_state'] = np.load(goal_state_path)
         self.params['goal_state_position'] = np.loadtxt(goal_state_pos_path)
         print(f"aiming for goal location {self.params['goal_state_position']}")
--- resources/monte_info/README.md
+++ resources/monte_info/README.md
@@ -2,10 +2,10 @@
 
 # goal state
 the goal state is the botton of the first ladder in monte
-`goal_state.npy`: image of agent in the goal state
-`goal_state_pos.txt`: x, y position of the goal state
+`middle_ladder.npy`: image of agent in the goal state
+`middle_ladder_pos.txt`: x, y position of the goal state
 
-`agent_space_goal_state.npy`: image of the agent's surrounding pixels in the goal state
+`middle_ladder_agent_space.npy`: image of the agent's surrounding pixels in the goal state
 
 # right ladder
 the right ladder in the ladder on the right side in the first room in monte
