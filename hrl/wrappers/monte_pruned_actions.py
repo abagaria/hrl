a773e997ab87074601ba7b3c791e90c372dd035f
Paul Zhou <paul_zhou@brown.edu>
2021-09-18 20:03:56 -0400

    [BUGFIX] override step() for new pruned_actions wrapper

--- hrl/wrappers/monte_pruned_actions.py
+++ hrl/wrappers/monte_pruned_actions.py
@@ -20,6 +20,23 @@ class MontePrunedActions(Wrapper):
 			'UPLEFTFIRE',
 		]
 		self.action_space = Discrete(len(self.meaningful_actions))
+		# overried unwrapped method for easier access
+		self.env.unwrapped.get_action_meanings = self.get_action_meanings
 	
 	def get_action_meanings(self):
 		return self.meaningful_actions
+	
+	def step(self, action):
+		if action not in self.action_space:
+			raise RuntimeError('action not in range for pruned actions')
+		action_to_original_action = {
+			0: 2,  # UP
+			1: 3,  # RIGHT
+			2: 4,  # LEFT
+			3: 5,  # DOWN
+			4: 10,  # UPFIRE
+			5: 13,  # UPRIGHTFIRE
+			6: 14,  # UPLEFTFIRE
+		}
+		original_action = action_to_original_action[action]
+		return self.env.step(original_action)
