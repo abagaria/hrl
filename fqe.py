697f0794030d617b81cd375ebc5f582ecf0bc18f
Omer Gottesman <omergott@gmail.com>
2022-05-03 22:30:21 -0400

    debug msg

--- fqe.py
+++ fqe.py
@@ -92,8 +92,8 @@ class FQE:
         state = torch.from_numpy(self.data["state"].astype(np.float32))
         done = torch.from_numpy(data["done"].astype(np.float32))
         target_q = reward
-        next_action = self.pi_eval(state)
         print('hi')
+        next_action = self.pi_eval(state)
         next_state_action = torch.cat(
             (torch.from_numpy(self.data["next_state"].astype(np.float32)), next_action), dim=1)
         self.loss_func = nn.MSELoss()
