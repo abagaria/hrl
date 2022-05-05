226d5f90175424ab3e6c7138bf43a3b19fc2f1ab
Omer Gottesman <omergott@gmail.com>
2022-05-05 14:48:53 -0400

    chunked policy action predictions

--- fqe.py
+++ fqe.py
@@ -28,46 +28,22 @@ class QFitter(nn.Module):
         value = self.linear_relu_stack(x)
         return value
 
-# def sample_batch(data, batch_size=50):
-#     zero_r_idx = np.where(data['reward'] == 0)
-#     sample_idx = zero_r_idx + random.sample(range(len(data["state"])), batch_size)
-#
-#     batch_data = {}
-#     batch_data['state'] = data['state'][sample_idx, :]
-#     batch_data['action'] = data['action'][sample_idx, :]
-#     batch_data['reward'] = data['reward'][sample_idx, :]
-#     batch_data['next_state'] = data['next_state'][sample_idx, :]
-#     return batch_data
+def chunked_policy_prediction(policy, states, action_dim, chunk_size=1000):
+    data_size = states.size(dim=0)
+    actions = torch.zeros((data_size, action_dim))
+    num_whole_chunks = data_size // chunk_size
+    for i in range(num_whole_chunks):
+        actions[i*chunk_size:(i+1)*chunk_size-1, :] = policy(states[i*chunk_size:(i+1)*chunk_size-1, :])
+    if data_size % chunk_size != 0:
+        actions[num_whole_chunks*chunk_size:, :] = policy(states[num_whole_chunks*chunk_size:, :])
+    return actions
+
+
 
 def generate_sample_idx(data, batch_size=50):
     zero_r_idx = np.where(data['reward'] == 1)
     sample_idx = list(zero_r_idx[0]) + random.sample(range(len(data["state"])), batch_size - len(zero_r_idx[0]))
     return sample_idx
-
-def optimize_model(data, target_q, q_fitter, loss_func, optimizer, batch_size, num_epochs):
-    state_action = torch.from_numpy(np.concatenate((data["state"], data["action"]), axis=1).astype(np.float32))
-    for epoch in range(num_epochs):
-        # Sample a batch with the transitions reaching the goal
-        sample_idx = generate_sample_idx(data, batch_size=batch_size)
-
-        # Feed forward
-        q_pred = q_fitter(state_action[sample_idx, :].requires_grad_())
-
-        # Calculate the loss
-        loss = loss_func(q_pred, target_q[sample_idx])
-
-        # Backward propagation: caluclate gradients
-        loss.backward()
-
-        # Update the weights
-        optimizer.step()
-
-        # Clear gradients
-        optimizer.zero_grad()
-
-        if epoch % 10 == 0 or epoch == num_epochs-1:
-            print('epoch {}: loss = {}'.format(epoch, loss.item()))
-
 
 class FQE:
     def __init__(self,
@@ -76,33 +52,62 @@ class FQE:
                  learning_rate=0.01,
                  device='cpu',
                  exp_name="tmp"):
-        self.data = data
         self.pi_eval = pi_eval
         self.state_dim = data["state"].shape[1]
         self.action_dim = data["action"].shape[1]
+        self.data_size = data["action"].shape[0]
 
         self.q_fitter = QFitter(self.state_dim, self.action_dim).to(device)
 
         self.learning_rate = learning_rate
         self.exp_name = exp_name
 
-
-    def fit(self, num_iter, gamma, batch_size, num_epochs, save_interval=np.inf):
-        reward = torch.from_numpy(self.data["reward"].astype(np.float32)).view(-1, 1)
-        state = torch.from_numpy(self.data["state"].astype(np.float32))
-        done = torch.from_numpy(data["done"].astype(np.float32))
-        target_q = reward
-        next_action = self.pi_eval(state)
-        next_state_action = torch.cat(
-            (torch.from_numpy(self.data["next_state"].astype(np.float32)), next_action), dim=1)
         self.loss_func = nn.MSELoss()
         self.optimizer = torch.optim.Adam(self.q_fitter.parameters(), lr=self.learning_rate)
-        for iter in range(num_iter):
-            print('Iteration: {}'.format(iter))
-            optimize_model(self.data, target_q, self.q_fitter, self.loss_func, self.optimizer, batch_size, num_epochs)
-            if (save_interval < np.inf and iter % save_interval == 0) or iter == num_iter-1:
-                torch.save(self.q_fitter.state_dict(), "saved_results/{}/weights_{}".format(self.exp_name, iter))
-            target_q = reward + (1. - done) * gamma * self.q_fitter(next_state_action).detach()
+
+        self.reward = torch.from_numpy(data["reward"].astype(np.float32)).view(-1, 1)
+        self.state = torch.from_numpy(data["state"].astype(np.float32))
+        self.state_action = torch.from_numpy(np.concatenate((data["state"], data["action"]), axis=1).astype(np.float32))
+        self.done = torch.from_numpy(data["done"].astype(np.float32))
+        next_action = chunked_policy_prediction(self.pi_eval, self.state, self.action_dim)
+        self.next_state_action = torch.cat(
+            (torch.from_numpy(data["next_state"].astype(np.float32)), next_action), dim=1)
+
+    def optimize_model(self, gamma, batch_size, num_batches):
+
+        for idx_batch in range(num_batches):
+            # Sample a batch with the transitions reaching the goal
+            sample_idx = generate_sample_idx(data, batch_size=batch_size)
+
+            # Feed forward
+            q_pred = self.q_fitter(self.state_action[sample_idx, :].requires_grad_())
+
+            # Compute target
+            target_q = self.reward[sample_idx, :] + (1. - self.done[sample_idx, :]) * gamma * self.q_fitter(
+                self.next_state_action[sample_idx, :]).detach()
+
+            # Calculate the loss
+            loss = self.loss_func(q_pred, target_q)
+
+            # Backward propagation: caluclate gradients
+            loss.backward()
+
+            # Update the weights
+            self.optimizer.step()
+
+            # Clear gradients
+            self.optimizer.zero_grad()
+
+            if idx_batch % 10 == 0 or idx_batch == num_batches - 1:
+                print('Batch {}: loss = {}'.format(idx_batch, loss.item()))
+
+    def fit(self, num_iter, gamma, batch_size, num_batches, save_interval=np.inf):
+        for iteration in range(num_iter):
+            print('Iteration: {}'.format(iteration))
+            self.optimize_model(gamma, batch_size, num_batches)
+            if (save_interval < np.inf and iteration % save_interval == 0) or iteration == num_iter-1:
+                torch.save(self.q_fitter.state_dict(), "saved_results/{}/weights_{}".format(self.exp_name, iteration))
+
 
     def predict(self, state):
         next_action = self.pi_eval(state)
@@ -112,7 +117,6 @@ class FQE:
 
 
 if __name__ == '__main__':
-    print('start')
     t0 = time.time()
     parser = argparse.ArgumentParser()
     parser.add_argument('--exp_name', type=str, default='tmp')
@@ -120,8 +124,8 @@ if __name__ == '__main__':
     parser.add_argument('--learning_rate', type=float, default=0.01)
     parser.add_argument('--num_iter', type=int, default=200)
     parser.add_argument('--gamma', type=float, default=0.995)
-    parser.add_argument('--batch_size', type=int, default=10000)
-    parser.add_argument('--num_epochs', type=int, default=100)
+    parser.add_argument('--batch_size', type=int, default=512)
+    parser.add_argument('--num_batches', type=int, default=100)
     parser.add_argument('--device', type=str, default='cpu')
     args = parser.parse_args()
 
@@ -162,19 +166,16 @@ if __name__ == '__main__':
     if not os.path.exists('saved_results/{}/'.format(args.exp_name)):
         os.makedirs('saved_results/{}/'.format(args.exp_name))
 
-    print('Created folder.')
     fqe = FQE(data,
               agent.actor,
               learning_rate=args.learning_rate,
               exp_name=args.exp_name,
               device=args.device)
-    print('Created fqe.')
     fqe.fit(args.num_iter,
             gamma=args.gamma,
             batch_size=args.batch_size,
-            num_epochs=args.num_epochs,
+            num_batches=args.num_batches,
             save_interval=args.save_interval)
-    print('Fitted fqe.')
 
     with open('saved_results/{}/args.txt'.format(args.exp_name), 'w') as f:
         for arg in vars(args):
@@ -183,4 +184,3 @@ if __name__ == '__main__':
         t1 = time.time()
         f.write("\n")
         f.write("Runtime: {0:.2f} minutes.".format((t1-t0)/60))
-    print('done')
