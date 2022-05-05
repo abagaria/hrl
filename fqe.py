40f4c154b625034c7342c719318bf372c5965716
Omer Gottesman <omergott@gmail.com>
2022-05-05 15:49:20 -0400

    dbg

--- fqe.py
+++ fqe.py
@@ -35,7 +35,8 @@ def chunked_policy_prediction(policy, states, action_dim, chunk_size=100):
     actions = torch.zeros((data_size, action_dim))
     num_whole_chunks = data_size // chunk_size
     for i in range(num_whole_chunks):
-        actions[i*chunk_size:(i+1)*chunk_size-1, :] = policy(states[i*chunk_size:(i+1)*chunk_size-1, :])
+        # actions[i*chunk_size:(i+1)*chunk_size-1, :] = policy(states[i*chunk_size:(i+1)*chunk_size-1, :])
+        a = policy(states[i*chunk_size:(i+1)*chunk_size-1, :])
     pdb.set_trace()
     if data_size % chunk_size != 0:
         actions[num_whole_chunks*chunk_size:, :] = policy(states[num_whole_chunks*chunk_size:, :])
@@ -55,7 +56,6 @@ class FQE:
                  learning_rate=0.01,
                  device='cpu',
                  exp_name="tmp"):
-        pdb.set_trace()
         self.pi_eval = pi_eval
         self.state_dim = data["state"].shape[1]
         self.action_dim = data["action"].shape[1]
