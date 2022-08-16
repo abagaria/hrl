7e63242f01828f211a8501c532ac1271f872c140
Omer Gottesman <omergott@gmail.com>
2022-08-16 14:40:48 +0300

    test load_chain

--- hrl/ope/compute_init_set_ground_truth.py
+++ hrl/ope/compute_init_set_ground_truth.py
@@ -3,7 +3,7 @@ from hrl.ope.utils import load_chain
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--base_fname", type=str, require=True)
+parser.add_argument("--base_fname", type=str, required=True)
 args = parser.parse_args()
 
 chain = load_chain(args.base_fname)
