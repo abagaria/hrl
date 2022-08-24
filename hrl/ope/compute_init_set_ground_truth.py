ce0fae0351c61dc8a434bf070315f07325efed45
Omer Gottesman <omergott@gmail.com>
2022-08-24 13:00:21 -0400

    test load_chain

--- hrl/ope/compute_init_set_ground_truth.py
+++ hrl/ope/compute_init_set_ground_truth.py
@@ -14,8 +14,11 @@ global_option, chain = load_chain(args.base_fname)
 
 for i in range(10):
     subgoal = chain[0].get_goal_for_rollout()
+    initial_state_xy = chain[0].initiation_classifier.sample()
+    initial_state_xy= chain[0].extract_goal_dimensions(initial_state_xy)
     option_transitions, total_reward = chain[0].rollout(
         step_number=0,
         goal=subgoal,
-        initial_state_xy=np.array([2.0, 9.0]))
+        initial_state_xy=np.array(initial_state_xy)
+    )
     print(total_reward)
