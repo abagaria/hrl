84196a03c8faf981c7becdf00f15f93cd8b646fa
Omer Gottesman <omergott@gmail.com>
2022-08-24 13:14:25 -0400

    test load_chain

--- hrl/ope/compute_init_set_ground_truth.py
+++ hrl/ope/compute_init_set_ground_truth.py
@@ -12,8 +12,8 @@ args = parser.parse_args()
 
 global_option, chain = load_chain(args.base_fname)
 
-test_option = global_option
-num_tries = 400
+test_option = chain[1]
+num_tries = 100
 successes = 0
 for i in range(num_tries):
     subgoal = test_option.get_goal_for_rollout()
