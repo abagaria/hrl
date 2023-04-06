b8e1744fcd9c49639a1f7428d5bdf250dd747372
Akhil Bagaria <abagaria@hmc.edu>
2023-04-06 09:40:35 -0400

    Added a new architecture for initiation GVF in continuous action spaces.

--- hrl/agent/td3/model.py
+++ hrl/agent/td3/model.py
@@ -67,6 +67,43 @@ class Critic(nn.Module):
         q1 = F.relu(self.l2(q1))
         q1 = self.l3(q1)
         return q1
+    
+
+class BoundedCritic(nn.Module):
+    """Function that maps (s, a) -> [0, 1] using a sigmoid activation."""
+    
+    def __init__(self, state_dim, action_dim):
+        super(BoundedCritic, self).__init__()
+
+        # Q1 architecture
+        self.l1 = nn.Linear(state_dim + action_dim, 256)
+        self.l2 = nn.Linear(256, 256)
+        self.l3 = nn.Linear(256, 1)
+
+        # Q2 architecture
+        self.l4 = nn.Linear(state_dim + action_dim, 256)
+        self.l5 = nn.Linear(256, 256)
+        self.l6 = nn.Linear(256, 1)
+
+    def forward(self, state, action):
+        sa = torch.cat([state, action], 1)
+
+        q1 = F.relu(self.l1(sa))
+        q1 = F.relu(self.l2(q1))
+        q1 = torch.sigmoid(self.l3(q1))
+
+        q2 = F.relu(self.l4(sa))
+        q2 = F.relu(self.l5(q2))
+        q2 = torch.sigmoid(self.l6(q2))
+        return q1, q2
+
+    def Q1(self, state, action):
+        sa = torch.cat([state, action], 1)
+
+        q1 = F.relu(self.l1(sa))
+        q1 = F.relu(self.l2(q1))
+        q1 = torch.sigmoid(self.l3(q1))
+        return q1
 
 
 class SingleCritic(nn.Module):
